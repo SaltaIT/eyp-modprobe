1d22b4c386cc23ad403500d6285336b322e005c5
Jordi Prats <jprats@systemadmin.es>
2016-10-24 15:20:49 +0200

    int

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,6 +2,7 @@ define modprobe::install(
                           $modulename = $name,
                           $command = '/bin/false'
                         ) {
+
   file { "/etc/modprobe.d/${modulename}.conf":
     ensure  => 'present',
     owner   => 'root',
@@ -10,5 +11,4 @@ define modprobe::install(
     content => "install ${modulename} ${command}\n",
     require => Package[$modprobe::params::package_name],
   }
-
 }
