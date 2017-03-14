505b040737b1d2654722f7ce1192bad31c139f35
Jordi Prats <jprats@systemadmin.es>
2017-03-14 11:53:52 +0100

    acceptance tsting CIS

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -10,11 +10,47 @@ describe 'modprobe class' do
 
       class { 'modprobe': }
 
+      modprobe::install { 'cramfs':
+        filename => 'CIS',
+      }
+      modprobe::install { 'freevxfs':
+        filename => 'CIS',
+      }
+      modprobe::install { 'jffs2':
+        filename => 'CIS',
+      }
+      modprobe::install { 'hfs':
+        filename => 'CIS',
+      }
+      modprobe::install { 'hfsplus':
+        filename => 'CIS',
+      }
+      modprobe::install { 'squashfs':
+        filename => 'CIS',
+      }
+      modprobe::install { 'udf':
+        filename => 'CIS',
+      }
+      modprobe::install { 'vfat':
+        filename => 'CIS',
+      }
       EOF
 
       # Run it twice and test for idempotency
       expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    describe file("/etc/modprobe.d/CIS.conf") do
+      it { should be_file }
+      its(:content) { should match 'install cramfs /bin/true' }
+      its(:content) { should match 'install freevxfs /bin/true' }
+      its(:content) { should match 'install jffs2 /bin/true' }
+      its(:content) { should match 'install hfs /bin/true' }
+      its(:content) { should match 'install hfsplus /bin/true' }
+      its(:content) { should match 'install squashfs /bin/true' }
+      its(:content) { should match 'install udf /bin/true' }
+      its(:content) { should match 'install vfat /bin/true' }
     end
 
   end
