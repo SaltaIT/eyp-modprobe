0d03f02324b5c332c44ce621ee613e077e33cb26
Jordi Prats <jprats@systemadmin.es>
2017-03-14 11:49:38 +0100

    multiplke installs in a single file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 0.1.4
+
+* modprobe::install modified to use concat to allow multiple installs using a single file
+
 ## 0.1.3
 
 * **modprobe::install** changed default command value (/bin/false) to **/bin/true**
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,15 +1,27 @@
 define modprobe::install(
                           $filename   = $name,
                           $modulename = $name,
-                          $command    = '/bin/true'
+                          $command    = '/bin/true',
+                          $order      = '00',
                         ) {
 
-  file { "/etc/modprobe.d/${filename}.conf":
-    ensure  => 'present',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "install ${modulename} ${command}\n",
-    require => Package[$modprobe::params::package_name],
+  if(!defined(Concat[""/etc/modprobe.d/${filename}.conf""]))
+  {
+    concat { "/etc/modprobe.d/${filename}.conf":
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package[$modprobe::params::package_name],
+    }
   }
+
+  concat::fragment{ "/etc/modprobe.d/${filename}.conf ${modulename}":
+    target  => "/etc/modprobe.d/${filename}.conf",
+    order   => $order,
+    content => "install ${modulename} ${command}\n",
+  }
+
+
+
 }
